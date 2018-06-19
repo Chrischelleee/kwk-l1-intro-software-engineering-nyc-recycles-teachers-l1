c77ae181b9f50fb0e527a15d60db9983782599f8
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-19 19:46:44 +0000

    Done.

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -1,8 +1,8 @@
 ###### Code Your Solutions Here!!!
 
 #Your code should work with a hash that looks like the one below (in comments).
-#The specs (which call your tests) are using these exact hashes 
-# We haven't hard coded the hashes in the tests, 
+#The specs (which call your tests) are using these exact hashes
+# We haven't hard coded the hashes in the tests,
 #they're dynamically brought in from the NYC Open Data API
 
 # first_recycle_bin =  {
@@ -17,18 +17,35 @@
 
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
-
+def address(first_recycling_bin)
+  return first_recycling_bin["address"]
+end
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
-
+def borough(first_recycling_bin)
+  return first_recycling_bin["borough"]
+end
 #3. Define a method called `all_keys` that takes a hash as an argument.
-#This method should print out each key in the hash. 
+#This method should print out each key in the hash.
 #You should use the `each_key` method to iterate over your hash and print out each key.
-
+def all_keys(first_recycling_bin)
+  first_recycling_bin.each_key do |keys|
+    puts "#{keys}"
+  end
+end
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
 #You should use the `each_value` method to iterate over your hash and print out each value.
-
+def all_data(second_recycling_bin)
+  second_recycling_bin.each_value do |value|
+    puts "#{value}"
+  end
+end
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
+def key_and_data(fourth_recycling_bin)
+  fourth_recycling_bin.each do |keys, value|
+    puts "The #{keys} is #{value}"
+  end
+end
